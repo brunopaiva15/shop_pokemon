fe17cf716cf66bc476af4dce25300a87e48508dc
Bruno Vergasta Paiva <ppaiva11144@gmail.com>
2025-05-23 19:52:57 +0200

    Update terms.php

--- terms.php
+++ terms.php
@@ -4,11 +4,6 @@ session_start();
 
 require_once 'includes/functions.php';
 
-if (!isset($_SESSION['authenticated']) || $_SESSION['authenticated'] !== true) {
-    header('Location: auth.php');
-    exit;
-}
-
 $pageTitle = 'Conditions Générales de Vente';
 require_once 'includes/header.php';
 ?>
